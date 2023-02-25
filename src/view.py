490e162f7b735a943f8850a60f8bf67d6009d52d
Masa-Asada <masayoshi.1011@gmail.com>
2023-02-25 12:53:15 +0900

    add input form

--- src/view.py
+++ src/view.py
@@ -6,11 +6,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # --- View 側の設定 ---
+
 # ルートディレクトリにアクセスした場合の挙動
 @app.route('/')
+
+# defいかがアクセス後の操作
 def index():
-	# return 'Hello World!'
-	return render_template('index.html')
+	# DBから以下の変数を読み込んできたと仮定
+	title_ = 'ようこそ'
+	message_ = 'MTVデザインパターンでWebアプリ作成'
+
+	return render_template('index.html', title=title_, message=message_)
+
+	# # return 'Hello World!'
+	# return render_template('index.html')
 
 # エントリーポイント
 if __name__ == '__main__':
